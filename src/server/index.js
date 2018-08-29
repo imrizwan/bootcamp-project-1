7b825a77318a3ee7ec7824495d8ad44df76954ff
imrizwan <razzyshaikh@gmail.com>
2018-08-30 03:57:16 +0500

    Heroku

--- src/server/index.js
+++ src/server/index.js
@@ -55,10 +55,10 @@ require('./routes')(app);
 //  response.status(404).send("Page not found!");
 //});
 
-app.use(express.static(path.join('..', __dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join('../', __dirname, '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 
