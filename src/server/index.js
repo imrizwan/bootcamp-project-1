3a41ba9f15a399d95c1cf85aca59c5998124acf7
imrizwan <razzyshaikh@gmail.com>
2018-08-30 03:49:39 +0500

    Heroku

--- src/server/index.js
+++ src/server/index.js
@@ -55,7 +55,12 @@ require('./routes')(app);
 //  response.status(404).send("Page not found!");
 //});
 
-app.use(express.static(path.join('..', '..', __dirname, 'dist')));
+app.use(express.static(path.join('..', __dirname, 'dist')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join('..', __dirname, 'dist', 'index.html'));
+});
+
 
 io.on('connection', socket => {
   console.log('User connected')
