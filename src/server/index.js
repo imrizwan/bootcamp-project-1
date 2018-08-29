881aaf0fa4108ba08722d55420a3ed2dcef5c3e7
imrizwan <razzyshaikh@gmail.com>
2018-08-30 03:52:44 +0500

    Heroku

--- src/server/index.js
+++ src/server/index.js
@@ -58,7 +58,7 @@ require('./routes')(app);
 app.use(express.static(path.join('..', __dirname, 'dist')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join('..', __dirname, 'dist', 'index.html'));
+  res.sendFile(path.join('../', __dirname, '/dist/index.html'));
 });
 
 
