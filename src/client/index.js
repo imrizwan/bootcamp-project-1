de0ead3681963d7208631f593fbccf5f58809633
imrizwan <razzyshaikh@gmail.com>
2018-08-31 13:38:52 +0500

    Dist

--- src/client/index.js
+++ src/client/index.js
@@ -5,27 +5,10 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap";
 import "popper.js/dist/popper.min.js";
 import "jquery/dist/jquery.slim.min.js";
-import { Provider } from "react-redux";
-import configureStore from "./store/store";
 import AppRoute from "./routes/routes";
-//import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 import registerServiceWorker from './registerServiceWorker';
-
-// if ('serviceWorker' in navigator) {
-//   const registration = runtime.register();
-//   registration
-//     .then(function () {
-//       console.log("Service Worker Registered!");
-//     });
-// }
-
-
-const store = configureStore();
-
 const jsx = (
-  <Provider store={store}>
-    <AppRoute />
-  </Provider>
+  <AppRoute />
 );
 
 ReactDOM.render(jsx, document.getElementById("root"));
